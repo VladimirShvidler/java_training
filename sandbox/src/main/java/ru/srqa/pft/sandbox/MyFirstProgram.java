a1ae12bb64251074276ef918fcc9a4888c023f1a
VladimirShvidler <vshvidler@lt-vshvidler.websense.com>
2016-02-29 19:09:35 +0200

    Added several functions

--- sandbox/src/main/java/ru/srqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/srqa/pft/sandbox/MyFirstProgram.java
@@ -3,6 +3,29 @@ package ru.srqa.pft.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    System.out.println("Hello, world!");
+    hello("world");
+
+    double l = 5;
+    System.out.println("Square's area with side " + l + " = " + area(l));
+
+    double a = 4;
+    double b = 6;
+    System.out.println("Rectangle's are with sides " + a + " and " + b + " = " + area(a, b));
+
+
+
   }
+
+  public static void hello(String somebody){
+        System.out.println("Hello " + somebody + "!");
+  }
+
+  public static double area(double len){
+    return len*len;
+  }
+
+  public static double area(double a, double b){
+    return a * b;
+  }
+
 }
